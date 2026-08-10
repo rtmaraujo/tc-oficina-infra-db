85c43d0f6178fc679bb832bb0c1e5fdc3e5e941e
Marcos Araujo <rtmaraujo@gmail.com>
2026-08-10 20:39:23 -0300

    style: terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,11 @@ resource "aws_security_group" "tc_db" {
   vpc_id      = data.aws_vpc.tc.id
 
   ingress {
-    description     = "Acesso a partir do security group da aplicacao no EKS"
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    cidr_blocks     = var.allowed_cidr_blocks
+    description = "Acesso a partir do security group da aplicacao no EKS"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidr_blocks
   }
 
   egress {
@@ -43,17 +43,17 @@ resource "aws_db_instance" "tc" {
   username = var.db_username
   password = var.db_password
 
-  allocated_storage     = var.allocated_storage
-  storage_type          = "gp3"
-  storage_encrypted     = true
+  allocated_storage       = var.allocated_storage
+  storage_type            = "gp3"
+  storage_encrypted       = true
   backup_retention_period = var.backup_retention_days
-  multi_az              = var.multi_az
-  publicly_accessible   = false
+  multi_az                = var.multi_az
+  publicly_accessible     = false
 
   db_subnet_group_name   = aws_db_subnet_group.tc.name
   vpc_security_group_ids = [aws_security_group.tc_db.id]
 
-  skip_final_snapshot     = false
+  skip_final_snapshot       = false
   final_snapshot_identifier = "tc-oficina-db-final"
 
   tags = {
